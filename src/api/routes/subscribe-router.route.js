f047d8fddd16ca0bdf83eb5ba2e30be53d51a7d7
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-03 13:43:21 +0300

    Add update & delete subscriber

--- src/api/routes/subscribe-router.route.js
+++ src/api/routes/subscribe-router.route.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { subscriberService } from '../../services/subscriber.service.js';
-import { responseMiddleware, createSubscriberValidMiddleware } from '../../middlewares/middlewares.js';
+import { 
+  responseMiddleware, 
+  createSubscriberValidMiddleware, 
+  updateSubscriberValidMiddleware
+} from '../../middlewares/middlewares.js';
 
 const router = Router();
 
@@ -8,6 +12,28 @@ router.route('/')
   .post(createSubscriberValidMiddleware, (req, res, next) => {
     try {
       const subscriber = subscriberService.addSubscriber(req.body);
+      res.data = subscriber;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  });
+
+router.route('/:id')
+  .put(updateSubscriberValidMiddleware, (req, res, next) => {
+    try {
+      const subscriber = subscriberService.updateSubscriber(req.params.id, req.body);
+      res.data = subscriber;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  })
+  .delete((req, res, next) => {
+    try {
+      const subscriber = subscriberService.deleteSubscriber(req.params.id);
       res.data = subscriber;
     } catch (err) {
       res.err = err;
